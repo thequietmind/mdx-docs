62192fc92e2b30a646909602c924d8c1268ea3d0
Ezra Free <ezrafree@gmail.com>
2025-06-24 20:22:51 -0600

    chore(package): set base config

---
+++ .vite/deps/@mui_icons-material_Menu.js
@@ -0,0 +1,20 @@
+"use client";
+import "./chunk-C6WWHQR7.js";
+import {
+  createSvgIcon,
+  require_jsx_runtime
+} from "./chunk-Y4FAKSHF.js";
+import "./chunk-2KHBIA62.js";
+import {
+  __toESM
+} from "./chunk-VCDLJVZS.js";
+
+// node_modules/@mui/icons-material/esm/Menu.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var Menu_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"
+}), "Menu");
+export {
+  Menu_default as default
+};
+//# sourceMappingURL=@mui_icons-material_Menu.js.map
