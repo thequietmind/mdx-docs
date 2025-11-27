db4a71540e278fe5834da8a7ffc0f7d15604a079
Ezra Free <ezrafree@gmail.com>
2025-11-27 12:22:04 -0700

    chore(app): add more components

--- src/config/pages.js
+++ src/config/pages.js
@@ -1,6 +1,11 @@
+import AlertMDX from "../contents/alert.mdx";
 import ButtonMDX from "../contents/button.mdx";
+import CardMDX from "../contents/card.mdx";
+import ChipMDX from "../contents/chip.mdx";
 import ColorsMDX from "../contents/colors.mdx";
 import HomeMDX from "../contents/home.mdx";
+import TabsMDX from "../contents/tabs.mdx";
+import TextFieldMDX from "../contents/textfield.mdx";
 import TypographyMDX from "../contents/typography.mdx";
 
 // Centralized pages configuration
@@ -13,14 +18,39 @@ export const pages = [
     isDefault: true, // This will be used for the catch-all route
   },
   {
+    name: "Alert",
+    route: "/alert",
+    component: AlertMDX,
+  },
+  {
     name: "Button",
     route: "/button",
     component: ButtonMDX,
   },
   {
+    name: "Card",
+    route: "/card",
+    component: CardMDX,
+  },
+  {
+    name: "Chip",
+    route: "/chip",
+    component: ChipMDX,
+  },
+  {
     name: "Colors",
     route: "/colors",
     component: ColorsMDX,
+  },
+  {
+    name: "Tabs",
+    route: "/tabs",
+    component: TabsMDX,
+  },
+  {
+    name: "TextField",
+    route: "/textfield",
+    component: TextFieldMDX,
   },
   {
     name: "Typography",
