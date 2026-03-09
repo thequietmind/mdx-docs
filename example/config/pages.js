c5da4ef1d2ebe358c3b7d8678fe5eab4d3cb3ff9
Ezra Free <ezrafree@gmail.com>
2026-03-09 13:51:47 -0600

    update demo content

--- example/config/pages.js
+++ example/config/pages.js
@@ -1,6 +1,14 @@
 import { lazy } from "react";
 
 const HomeMDX = lazy(() => import("@pages/home.mdx"));
+const AlertMDX = lazy(() => import("@pages/alert.mdx"));
+const ButtonMDX = lazy(() => import("@pages/button.mdx"));
+const CardMDX = lazy(() => import("@pages/card.mdx"));
+const ChipMDX = lazy(() => import("@pages/chip.mdx"));
+const ColorsMDX = lazy(() => import("@pages/colors.mdx"));
+const TabsMDX = lazy(() => import("@pages/tabs.mdx"));
+const TextFieldMDX = lazy(() => import("@pages/textfield.mdx"));
+const TypographyMDX = lazy(() => import("@pages/typography.mdx"));
 
 export const pages = [
   {
@@ -8,5 +16,45 @@ export const pages = [
     route: "/",
     component: HomeMDX,
     isDefault: true,
+  },
+  {
+    name: "Alert",
+    route: "/alert",
+    component: AlertMDX,
+  },
+  {
+    name: "Button",
+    route: "/button",
+    component: ButtonMDX,
+  },
+  {
+    name: "Card",
+    route: "/card",
+    component: CardMDX,
+  },
+  {
+    name: "Chip",
+    route: "/chip",
+    component: ChipMDX,
+  },
+  {
+    name: "Colors",
+    route: "/colors",
+    component: ColorsMDX,
+  },
+  {
+    name: "Tabs",
+    route: "/tabs",
+    component: TabsMDX,
+  },
+  {
+    name: "TextField",
+    route: "/textfield",
+    component: TextFieldMDX,
+  },
+  {
+    name: "Typography",
+    route: "/typography",
+    component: TypographyMDX,
   },
 ];
